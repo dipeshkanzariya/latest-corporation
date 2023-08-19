b05053270f6f275d11cb7d0f4920eae32746015f
Dipesh Kanzariya <21010101098@darshan.ac.in>
2023-08-19 12:31:50 +0530

    dipesh-folder structure

---
+++ src/Components/aboutUs.js
@@ -0,0 +1,11 @@
+function AboutUs() {
+    return (
+        <>
+            <h1>
+                About Page
+            </h1>
+        </>
+    )
+}
+
+export default AboutUs
--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './Layout/layout';
+import Home from './Components/home';
+import AboutUs from './Components/aboutUs';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Layout />}></Route>
+          <Route path="/" element={<Layout />}>
+            <Route index element = {<Home />}></Route>
+            <Route path='/aboutUs' element = {<AboutUs />}></Route>
+          </Route>
       </Routes>
     </BrowserRouter>
   </>
