6f55c4da184b27b417683eb91aa59b731c626b56
Aditya Jogia <21010101082@darshan.ac.in>
2023-08-21 21:10:50 +0530

    carousel and header changes

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,7 +1,32 @@
-function Home(){
-    return(
+function Home() {
+    return (
         <>
-        <h1>Home Page</h1>
+            <div id="carouselExampleIndicators" className="carousel slide">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active ">
+                        <img src="https://latestcorporation.com/assets/img/slide/a.jpeg" className="d-block carouselimg w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://latestcorporation.com/assets/img/slide/b.jpeg" className="d-block carouselimg w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://latestcorporation.com/assets/img/slide/c.jpg" className="d-block carouselimg w-100" alt="..." />
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
         </>
     )
 }
--- src/Layout/header.js
+++ src/Layout/header.js
@@ -3,36 +3,41 @@ import { Link } from "react-router-dom";
 function Header() {
     return (
         <>
-            <nav class="navbar navbar-expand-lg bg-light navbar-light header" >
-                <div class="container">
-                    <Link class="navbar-brand" to="/"><img className="logo" src="https://latestcorporation.com/assets/img/logol.png" alt=""/></Link>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+        <div className="header-div">
+
+            <nav className="navbar navbar-expand-lg bg-light navbar-light header " >
+                <div className="container">
+                    <Link className="navbar-brand" to="/"><img className="logo" src="https://latestcorporation.com/assets/img/logol.png" alt=""/></Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" to="/aboutUs">About</Link>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/aboutUs">About</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" to="/contact">Contact Us</Link>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/contact">Contact Us</Link>
                             </li>
-                            <li class="nav-item dropdown">
-                                <Link class="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <li className="nav-item dropdown">
+                                <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Products
                                 </Link>
-                                <ul class="dropdown-menu">
-                                    <li><Link class="dropdown-item" to="/products/brass">Brass</Link></li>
-                                    <li><Link class="dropdown-item" to="/products/kitchensink">Kitchen Sink</Link></li>     
+                                <ul className="dropdown-menu">
+                                    <li><Link className="dropdown-item" to="/products/brass">Brass</Link></li>
+                                    <li><Link className="dropdown-item" to="/products/kitchensink">Kitchen Sink</Link></li>     
                                 </ul>
                             </li>
                         </ul>
                     </div>
+                   
                 </div>
             </nav>
+        </div>
+        
         </>
     );
 }
